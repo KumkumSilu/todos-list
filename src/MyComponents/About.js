529f9ecbae61ebec79d8c114add6b9885815ea4d
kumkumsilu <kumkumsahoo2001@gmail.com>
2025-07-01 12:30:01 +0530

    Initial commit: React Todo App

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>
+  I'm Kumkum Sahoo, a Frontend Developer. This app reflects my hands-on experience with React and showcases my focus on building modular, maintainable UI components.
+</p>
+        </div>
+    )
+}
